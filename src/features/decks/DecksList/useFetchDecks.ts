ae51816d4141d19958c065945ab24fb63b61533a
matveev96 <sasha.matveev1996@icloud.com>
2025-02-13 14:55:31 +0300

    feat: add isLoading to use for skeleton wrapper

--- src/features/decks/DecksList/useFetchDecks.ts
+++ src/features/decks/DecksList/useFetchDecks.ts
@@ -1,6 +1,6 @@
 import { useAppDispatch, useAppSelector } from '../../../app/store.ts'
 import { selectDecks } from '../decks-selectors.ts'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { fetchDecksTC } from '../decks-thunks.ts'
 import { selectApp } from '../../../app/app-selectors.ts'
 import { setLoadingDeckAC } from '../../../app/app-reducer.ts'
@@ -11,17 +11,24 @@ export const useFetchDecks = () => {
   const decks = useAppSelector(selectDecks)
   const app = useAppSelector(selectApp)
 
-  useEffect(() => {
-    (async () => {
-      dispatch(setLoadingDeckAC('loading'))
-      await dispatch(fetchDecksTC())
-      dispatch(setLoadingDeckAC('succeeded'))
-    })()
+  const [isLoading, setIsLoading] = useState(true)
 
+  useEffect(() => {
+    // (async () => {
+    //   dispatch(setLoadingDeckAC('loading'))
+    //   await dispatch(fetchDecksTC())
+    //   dispatch(setLoadingDeckAC('succeeded'))
+    // })()
+    dispatch(setLoadingDeckAC('loading'))
+    dispatch(fetchDecksTC()).finally(() => {
+      setIsLoading(false)
+      dispatch(setLoadingDeckAC('succeeded'))
+    })
   }, [dispatch])
 
   return {
     decks,
-    app
+    app,
+    isLoading
   }
 }
