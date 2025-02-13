ddf3d01337d479d8d9edbf0fd4a64418a20a4c87
matveev96 <sasha.matveev1996@icloud.com>
2025-02-13 14:57:44 +0300

    feat: add footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,6 +14,7 @@ export const App = () => {
       {app.status === 'loading' ? <LinearLoader/> : null}
       <Decks />
       <GlobalError />
+      <footer>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus at dicta distinctio earum enim esse, expedita harum inventore molestias nam nesciunt numquam placeat possimus, quisquam repudiandae sit suscipit ut voluptatum.</footer>
     </div>
   )
 }
