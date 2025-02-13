9e23eb698e40f8366a37eb476f30026f46c46a41
matveev96 <sasha.matveev1996@icloud.com>
2025-02-13 14:56:01 +0300

    feat: add skeleton wrapper when isLoading is true

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -2,14 +2,21 @@ import s from './DecksList.module.css'
 import { DeckItem } from './DeckItem/DeckItem.tsx'
 import { useFetchDecks } from './useFetchDecks.ts'
 
+import { DeckItemSkeleton } from './DeckItem/DeckItemSkeleton.tsx'
+
+
 export const DecksList = () => {
-  const { decks } = useFetchDecks()
+  const { decks, isLoading } = useFetchDecks()
 
   return (
-    <ul className={s.list}>
-      {decks.map((deck) => (
-        <DeckItem key={deck.id} deck={deck} />
-      ))}
-    </ul>
+    <>
+      <ul className={s.list}>
+        {isLoading && decks.length === 0 && <DeckItemSkeleton count={10}/>}
+        {decks.map((deck) => (
+          <DeckItem key={deck.id} deck={deck} />
+        ))}
+      </ul>
+    </>
+
   )
 }
