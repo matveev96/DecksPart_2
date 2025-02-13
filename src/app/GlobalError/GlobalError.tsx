d25917bf9410979c90970e33f2b5cfd4f7f4ccb7
matveev96 <sasha.matveev1996@icloud.com>
2025-02-13 14:59:12 +0300

    feat: add errorMessage to render Error

--- src/app/GlobalError/GlobalError.tsx
+++ src/app/GlobalError/GlobalError.tsx
@@ -1,8 +1,12 @@
 import { useEffect } from 'react'
 import { toast, ToastContainer } from 'react-toastify'
+import { useFetchDecks } from '../../features/decks/DecksList/useFetchDecks.ts'
 
 export const GlobalError = () => {
-  const errorMessage = ''
+
+  const {app} = useFetchDecks()
+
+  const errorMessage = app.error
 
   useEffect(() => {
     if (errorMessage) {
